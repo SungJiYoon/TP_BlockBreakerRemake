16bcf9f15579dd66305b7d0e4182165eb149ce17
yoos99 <yoosh2520@naver.com>
2022-11-20 15:26:50 +0900

    first pompomyounji

--- app/src/main/java/com/example/owner/gameactivity/MainActivity.java
+++ app/src/main/java/com/example/owner/gameactivity/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     //전역적으로 사용될 변수
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //게임시작 버튼 클릭시 액티비티 전환
-        Button name_rule_btn = (Button) findViewById(R.id.start_btn);
+        ImageButton name_rule_btn = (ImageButton) findViewById(R.id.start_btn);
         name_rule_btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
